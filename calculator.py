6baa74ee74bd8daa1c06cdbfb6b9ef0a8eeb27f2
mittem <sofipanovi@gmail.com>
2025-03-28 11:36:52 +0300

    Added functiond descriptions

--- calculator.py
+++ calculator.py
@@ -1,20 +1,38 @@
 def add(x, y):
+
+    '''Вычисление суммы двух чисел'''
+
     return x + y
 
 def subtract(x, y):
+
+    '''Вычисление разности двух чисел'''
+
     return x - y
 
 def multiply(x, y):
+
+    '''Вычисление произведения двух чисел'''
+
     return x * y
 
 def divide(x, y):
+
+    '''Вычисление частного двух чисел'''
+
     if y == 0:
         raise ValueError("Cannot divide by zero!")
     return x / y
 def power(x, y):
+
+    '''Вычисление результата возведения заданного числа в заданную степень'''
+
     return x ** y
 
 def sqrt(x):
+
+    '''Вычисление результата извлечения квадратного корня из заданного числа'''
+
     if x < 0:
         raise ValueError("Cannot compute square root of a negative number!")
     return x ** 0.5
